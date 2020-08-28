e26b30f1b22f9c52e1cda8e91603bdeeafc10bf4
Koji <kk2796@nyu.edu>
2020-08-28 03:51:29 -0400

    update title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,14 +19,14 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} Koji Notion Blog</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
         <meta name="og:title" content="My Notion Blog" />
         <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta name="twitter:site" content="@koji_kanao" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
