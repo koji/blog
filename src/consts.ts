c6287d7e27f4c8950bcf459d377ca78139b4cb11
koji <kk2796@nyu.edu>
2024-09-13 13:01:29 +0900

    update tagline

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog'; // site meta description for SEO
+export const SITE_TAGLINE = 'Minimalist Blog'; // site tagline
+export const SITE_DESCRIPTION = 'Minimalist Blog'; // site meta description for SEO
