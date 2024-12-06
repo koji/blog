3b63b0e1e35646c462ce2b1ad50d84bb0983ceb2
koji <baxin1919@gmail.com>
2024-12-06 01:01:27 -0500

    update constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Blog'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Blog'; // site meta description for SEO
+export const SITE_TITLE = 'koji blog';
+export const SITE_TAGLINE = 'software engineer Blog'; // site tagline
+export const SITE_DESCRIPTION = 'tech blog about frontend and gen ai'; // site meta description for SEO
