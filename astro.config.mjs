aa476957ff294a1cbb0a1a4e7fcdce996c022d6a
koji <kk2796@nyu.edu>
2026-06-24 04:06:20 -0400

    fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,28 +5,36 @@ import partytown from '@astrojs/partytown';
 import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
 import { optimizeMediaElements } from './src/optimizeMediaElements';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://baxin.netlify.app',
-	integrations: [
-		mdx(),
-		sitemap(),
-		partytown({
-			// Adds dataLayer.push as a forwarding-event.
-			config: {
-				forward: ['dataLayer.push']
-			}
-		})
-	],
-	markdown: {
-		rehypePlugins: [
-			[
-				autoNewTabExternalLinks,
-				{
-					domain: 'localhost:4321'
-				}
-			],
-			optimizeMediaElements
-		]
-	}
+  site: 'https://baxin.pages.dev/',
+
+  integrations: [
+      mdx(),
+      sitemap(),
+      partytown({
+          // Adds dataLayer.push as a forwarding-event.
+          config: {
+              forward: ['dataLayer.push']
+          }
+      })
+],
+
+  markdown: {
+      rehypePlugins: [
+          [
+              autoNewTabExternalLinks,
+              {
+                  domain: 'localhost:4321'
+              }
+          ],
+          optimizeMediaElements
+      ]
+	},
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
